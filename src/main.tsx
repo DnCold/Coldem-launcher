8511333cf2d10aafdfb7eb1da497d78dfaf660b5
DanCold <danicosta96@gmail.com>
2026-08-12 17:56:02 -0300

    Polish native launcher interactions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/global.css";
 
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
